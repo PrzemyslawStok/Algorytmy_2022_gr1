4c8c16824199ff49d8f9f349167492559b01163b
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-04-09 10:41:04 +0200

    cd..

--- macierze_podzial.py
+++ macierze_podzial.py
@@ -1,4 +1,14 @@
 import numpy as np
 
+
+def funkcja1():
+    A = np.arange(1, 10)
+    print(A)
+    print(A[0])
+
+    for i in range(5, 9):
+        print(A[i])
+
+
 if __name__ == "__main__":
-    pass
+    funkcja1()
