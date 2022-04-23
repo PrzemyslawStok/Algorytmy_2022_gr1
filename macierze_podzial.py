43f62920692256c3644631484e489837d4b95d71
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-04-23 10:27:38 +0200

    cd..

--- macierze_podzial.py
+++ macierze_podzial.py
@@ -28,6 +28,32 @@ def funkcja2():
     print(B[3:8, 5:9])
 
 
+def funkcja3():
+    A = np.arange(0, 10)
+    # B = A.reshape([10, -1])
+
+    print(A)
+    print(A[0:2])
+    print(A[2:])
+
+    B = np.arange(1, 6)
+
+    print(B)
+    a = 2
+    print(B[:a])
+    print(B[a:])
+
+
+def podzial1(A: np.ndarray, a=90) -> (np.ndarray, np.ndarray):
+    A = np.zeros(10)
+    B = np.zeros(10)
+    return A, B
+
+
 if __name__ == "__main__":
     # funkcja1()
-    funkcja2()
+    # funkcja2()
+
+    A, B = podzial1(np.arange(0, 100), 50)
+    print(A)
+    print(B)
