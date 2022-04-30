7ceaa8ba5168d2110cd3b3011dae8c1dd9bbf02a
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-04-30 10:19:54 +0200

    cd..

--- wykresy.py
+++ wykresy.py
@@ -2,4 +2,11 @@ import numpy as np
 from matplotlib import pyplot as plot
 
 if __name__ == "__main__":
-    pass
+    X = np.arange(1, 10)
+    Y = X * X
+
+    print(X)
+    print(Y)
+
+    plot.plot(X, Y)
+    plot.show()
