f02c334e1eec67e42ca1e191688faf6504aae6f6
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-04-30 10:56:47 +0200

    cd..

--- wykresy.py
+++ wykresy.py
@@ -2,11 +2,12 @@ import numpy as np
 from matplotlib import pyplot as plot
 
 if __name__ == "__main__":
-    X = np.arange(1, 10)
+    X = np.linspace(-2, 2, 50)
     Y = X * X
 
-    print(X)
-    print(Y)
+    plot.plot(X, Y, label=rf"$y=x^2$")
+    Y = np.sin(X)
+    plot.plot(X, Y, label=rf"y=sin(x)")
 
-    plot.plot(X, Y)
+    plot.legend()
     plot.show()
