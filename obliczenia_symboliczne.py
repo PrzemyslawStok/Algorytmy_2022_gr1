ba7c6e8f7a3dd5834150b828ee08f70a5ac858f5
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-05-28 11:29:27 +0200

    cd..

--- obliczenia_symboliczne.py
+++ obliczenia_symboliczne.py
@@ -1,4 +1,13 @@
-from sympy import symbols
+from sympy import symbols, pprint, sqrt
+import sympy
 
 if __name__ == "__main__":
-    pass
+    x = symbols("x")
+    y = x * (x + 1) + sympy.sin(x)
+    pprint(y)
+    integral1 = sympy.integrate(y, x)
+    pprint(integral1)
+
+    y = (x + sympy.sin(x)) ** 10
+    integral2 = sympy.integrate(y, x)
+    pprint(integral2)
