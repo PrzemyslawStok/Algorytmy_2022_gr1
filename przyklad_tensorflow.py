b81921a4ac7a68552d9654fb29338efe27f956f7
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-06-11 10:32:14 +0200

    cd..

--- przyklad_tensorflow.py
+++ przyklad_tensorflow.py
@@ -1,4 +1,17 @@
 import tensorflow as tf
 
+import numpy as np
+from matplotlib import pyplot as plot
+
+
+def data() -> (np.ndarray, np.ndarray):
+    X = np.linspace(0, 2 * np.pi, 100)
+    Y = np.sin(X)
+
+    return X, Y
+
+
 if __name__ == "__main__":
-    pass
+    X, Y = data()
+    plot.plot(X, Y)
+    plot.show()
