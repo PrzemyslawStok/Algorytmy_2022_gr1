f227011fda9fbfbcaacb1ccf0e31a56413d1ca3f
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-06-18 10:21:48 +0200

    cd..

--- obliczenia_predkosc.py
+++ obliczenia_predkosc.py
@@ -1,4 +1,17 @@
 import timeit
 
+
+def mnozenie(l=100_000) -> list:
+    A = []
+    for i in range(l):
+        A.append(1)
+
+    for i in range(l):
+        for j in range(10):
+            A[i] = A[i] * 10
+
+    return A
+
+
 if __name__ == "__main__":
-    pass
+    A = mnozenie()
