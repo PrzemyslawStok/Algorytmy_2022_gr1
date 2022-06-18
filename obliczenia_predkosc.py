9e06c3351bb8ce8319dec4a0f00b3c88766dcadd
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-06-18 10:25:35 +0200

    cd..

--- obliczenia_predkosc.py
+++ obliczenia_predkosc.py
@@ -1,7 +1,8 @@
 import timeit
+import numpy as np
 
-
-def mnozenie(l=100_000) -> list:
+def mnozenie(l=100_000) -> (list, float):
+    start = timeit.default_timer()
     A = []
     for i in range(l):
         A.append(1)
@@ -10,8 +11,12 @@ def mnozenie(l=100_000) -> list:
         for j in range(10):
             A[i] = A[i] * 10
 
-    return A
+    end = timeit.default_timer()
+
+    return A, end - start
 
 
 if __name__ == "__main__":
-    A = mnozenie()
+    l = 100_000
+    A, czas = mnozenie(l)
+    print(f"obliczenia zajęły: {czas:0.2}s")
