494dce39ffd31a21189750b7441e84fa2a94ef5c
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-06-18 11:08:58 +0200

    cd..

--- obliczenia_predkosc.py
+++ obliczenia_predkosc.py
@@ -20,6 +20,19 @@ def mnozenie(l=100_000) -> (list, float):
     return A, end - start
 
 
+@jit
+def mnozenie_numba(l=100_000) -> (list, float):
+    A = []
+    for i in range(l):
+        A.append(1)
+
+    for i in range(l):
+        for j in range(10):
+            A[i] = A[i] * 10
+
+    return A, 0.0
+
+
 def mnozenie_numpy(l=100_000) -> (np.ndarray, float):
     start = timeit.default_timer()
 
@@ -46,7 +59,11 @@ def mnozenie_tensorflow(l=100_000) -> (np.ndarray, float):
 
 
 def zmierz_czas(f, l=100_000):
-    A, czas = f(l)
+    start = timeit.default_timer()
+    A, _ = f(l)
+    end = timeit.default_timer()
+    czas = end - start
+
     print(f"obliczenia zajęły: {czas:0.2}s")
 
 
@@ -55,3 +72,5 @@ if __name__ == "__main__":
     zmierz_czas(mnozenie, l)
     zmierz_czas(mnozenie_numpy, l)
     zmierz_czas(mnozenie_tensorflow, l)
+    zmierz_czas(mnozenie_numba, l)
+    zmierz_czas(mnozenie_numba, l)
