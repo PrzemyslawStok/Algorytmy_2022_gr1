b6bee152d28edb9c764a02f74b0bab571afa6ae6
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-06-18 10:29:42 +0200

    cd..

--- obliczenia_predkosc.py
+++ obliczenia_predkosc.py
@@ -1,6 +1,7 @@
 import timeit
 import numpy as np
 
+
 def mnozenie(l=100_000) -> (list, float):
     start = timeit.default_timer()
     A = []
@@ -16,7 +17,16 @@ def mnozenie(l=100_000) -> (list, float):
     return A, end - start
 
 
+def mnozenie_numpy(l=100_000) -> (np.ndarray, float):
+    return None, 0.0
+
+
+def zmierz_czas(f, l=100_000):
+    A, czas = f(l)
+    print(f"obliczenia zajęły: {czas:0.2}s")
+
+
 if __name__ == "__main__":
     l = 100_000
-    A, czas = mnozenie(l)
-    print(f"obliczenia zajęły: {czas:0.2}s")
+    zmierz_czas(mnozenie, l)
+    zmierz_czas(mnozenie_numpy, l)
