225c65313670cf72cbe9b1e83c25dc597803474e
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-06-18 11:27:57 +0200

    cd..

--- obliczenia_predkosc.py
+++ obliczenia_predkosc.py
@@ -5,8 +5,7 @@ import tensorflow as tf
 from numba import jit
 
 
-def mnozenie(l=100_000) -> (list, float):
-    start = timeit.default_timer()
+def mnozenie(l=100_000) -> list:
     A = []
     for i in range(l):
         A.append(1)
@@ -15,9 +14,7 @@ def mnozenie(l=100_000) -> (list, float):
         for j in range(10):
             A[i] = A[i] * 10
 
-    end = timeit.default_timer()
-
-    return A, end - start
+    return A
 
 
 @jit
@@ -33,38 +30,36 @@ def mnozenie_numba(l=100_000) -> (list, float):
     return A, 0.0
 
 
-def mnozenie_numpy(l=100_000) -> (np.ndarray, float):
-    start = timeit.default_timer()
-
+def mnozenie_numpy(l=100_000) -> np.ndarray:
     A = np.ones(l)
 
     for i in range(10):
         A = A + 10
 
-    end = timeit.default_timer()
-    return A, end - start
+    return A
 
 
 @tf.function
-def mnozenie_tensorflow(l=100_000) -> (np.ndarray, float):
-    start = timeit.default_timer()
-
+def mnozenie_tensorflow(l=100_000) -> np.ndarray:
     A = tf.ones(l)
 
     for i in range(10):
         A = A + 10
 
-    end = timeit.default_timer()
-    return A, end - start
+    return A
 
 
 def zmierz_czas(f, l=100_000):
     start = timeit.default_timer()
-    A, _ = f(l)
+    A = f(l)
     end = timeit.default_timer()
     czas = end - start
 
     print(f"obliczenia zajęły: {czas:0.2}s")
+
+
+def f1():
+    return 1.0, 2.0, 3.0
 
 
 if __name__ == "__main__":
@@ -74,3 +69,6 @@ if __name__ == "__main__":
     zmierz_czas(mnozenie_tensorflow, l)
     zmierz_czas(mnozenie_numba, l)
     zmierz_czas(mnozenie_numba, l)
+
+    a, _, b = f1()
+    print(f"a={a}, b={b}")
