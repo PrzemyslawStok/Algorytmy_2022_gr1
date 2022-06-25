ad1f8b4e6f541f47ea7c8a346177694f4b155f45
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-06-25 10:42:27 +0200

    cd..

--- funkcje_cwiczenia.py
+++ funkcje_cwiczenia.py
@@ -5,9 +5,26 @@ import tensorflow as tf
 from numba import jit
 
 
-def f0():
-    return 0
+def tuple_list():
+    A = (1, 2, 3, 4, 5, 6)
+    print(A)
+
+    A_list = list(A)
+
+    A_list[1] = 10
+
+    A_tuple = tuple(A_list)
+
+    for a in A_tuple:
+        print(a)
+
+
+def f0() -> (int, (int, int)):
+    return (10, (2, 5))
 
 
 if __name__ == "__main__":
-    f0()
+    a, b = f0()
+    print(f"a={a}, b={b}")
+
+    print(f0())
