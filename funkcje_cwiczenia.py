15ed302999f5b51c8e0fd5944014ec0302f54abf
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2022-06-25 11:02:50 +0200

    cd..

--- funkcje_cwiczenia.py
+++ funkcje_cwiczenia.py
@@ -23,8 +23,24 @@ def f0() -> (int, (int, int)):
     return (10, (2, 5))
 
 
+def f1():
+    return ((10, 5, (3, 2)), (2, 7))
+
+
 if __name__ == "__main__":
     a, b = f0()
     print(f"a={a}, b={b}")
 
     print(f0())
+
+    a, (b, c) = f0()
+
+    print(f"a={a}, b={b}, c={c}")
+
+    a, _ = f0()
+
+    print(f"a={a}")
+
+    a, (b, _) = f0()
+
+    print(f"a={a}, b={b}")
